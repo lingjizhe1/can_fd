d1bdc46b34cf17d7d8710f8bf13cd603272c0a1e
lingjizhe1 <lingjizhe1@gmail.com>
2025-08-08 16:07:13 +0800

    ignore

--- core1/hpm6750evkmini_flash_sdram_xip_debug/segger_embedded_studio/Output/Debug/Obj/tran_receTest_core1 - hpm6750evkmini/utils/hpm_crc32.c.d
+++
@@ -1,8 +0,0 @@
-Output/Debug/Obj/tran_receTest_core1\ -\ hpm6750evkmini/utils/hpm_crc32.c.o: \
- F:\HPM_MCU\sdk_env-main\hpm_sdk\utils\hpm_crc32.c \
- F:\HPM_MCU\sdk_env-main\hpm_sdk\utils\hpm_crc32.h \
- F:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/stdint.h \
- F:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/__SEGGER_RTL.h \
- F:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/__SEGGER_RTL_ConfDefaults.h \
- F:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/__SEGGER_RTL_Conf.h \
- F:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/__SEGGER_RTL_RISCV_Conf.h
